b24842be4800ac8e242102ef7b2e3f47e7a3e524
weselben <ben@weselben.de>
2023-04-30 13:07:40 +0200

    fix: fixed spliting inside words

--- discord/main.py
+++ discord/main.py
@@ -107,7 +107,7 @@ async def on_message(message):
             await message.reply(response, mention_author=False)
         elif len(response) >= int(2000):
             max_length = 2000
-            message_parts = [response[i:i + max_length] for i in range(0, len(response), max_length)]
+            message_parts = [response[i:i+max_length] for i in range(0, len(response), max_length) if response[i:i+max_length][-1] == ' ' or i+max_length >= len(response)]
 
             for part in message_parts:
                 await message.reply(part, mention_author=False)
